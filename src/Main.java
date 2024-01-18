df6985d02a8b91583c11a0d0db4989e31311ec95
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-18 19:47:58 +0300

    tests classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import cylinder.Circle;
 import cylinder.Cylinder;
+import cylinder.Rectangle;
+import pool.Cuboid;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +21,25 @@ public class Main {
 
         System.out.println("cylinder.volume= " + cylinder.getVolume());
         ;
+        System.out.println("*****************");
+        Rectangle rectangle = new Rectangle(5, 10);
+
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+
+        System.out.println("rectangle.length= " + rectangle.getLength());
+
+        System.out.println("rectangle.area= " + rectangle.getArea());
+
+        Cuboid cuboid = new Cuboid(5, 10, 5);
+
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+
+        System.out.println("cuboid.length= " + cuboid.getLength());
+
+        System.out.println("cuboid.area= " + cuboid.getArea());
+
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
     }
 }
