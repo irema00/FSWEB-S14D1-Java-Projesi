20125adc09f4196fa930e270165784a9cbe9db82
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-18 19:39:37 +0300

    creates cuboid class

---
+++ src/pool/Cuboid.java
@@ -0,0 +1,16 @@
+package pool;
+
+import cylinder.Rectangle;
+
+public class Cuboid extends Rectangle {
+    private int height;
+
+    public Cuboid(int width, int length, int height) {
+        super(width, length);
+        this.height = Math.max(height, 0);
+    }
+
+    public int getVolume() {
+        return super.getArea() * this.height;
+    }
+}
